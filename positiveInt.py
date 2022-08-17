62a7c0b60e5f9eb81e27716122bea4b073a99915
Rahulkhot10 <82927483+Rahulkhot10@users.noreply.github.com>
2022-08-17 16:46:06 +0530

    Add files via upload
    printing positive numbers from list

---
+++ positiveInt.py
@@ -0,0 +1,14 @@
+import importlib
+from pydoc import importfile
+importlib
+
+integerList=[12,-7,5,64,-14]
+integerList2=[12,14,-95,3]
+
+for number in integerList:
+    if number>0:
+        print(number)
+
+for number in integerList2:
+    if number>0:
+        print(number)
